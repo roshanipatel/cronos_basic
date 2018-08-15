3bd9cdb20972defc333f2f530278906a774b7ea5
roshani <roshanir6@gmail.com>
2018-08-15 21:58:53 +0530

    code commited

--- views/userProjectTask/index.php
+++ views/userProjectTask/index.php
@@ -1,7 +1,31 @@
-<h1>Tareas</h1>
+<?php
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-    'summaryText' => 'Mostrando {start}-{end} resultado(s)',
-)); ?>
+use yii\helpers\Html;
+use fedemotta\datatables\DataTables;
+use common\components\Common;
+use app\models\enums\Roles;
+?>
+<div class="row">
+    <div class="col-lg-12">
+		<h1 class="page-header">Tareas</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Tareas
+            </div>
+            <div class="panel-body">
+            	<?php
+				echo DataTables::widget([
+					'dataProvider'=>$dataProvider,
+					'itemView'=>'_view',
+				    'summaryText' => 'Mostrando {start}-{end} resultado(s)',
+				    ]);
+				    ?>
+			</div>
+		</div>
+	</div>
+</div>
+
